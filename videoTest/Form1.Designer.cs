131e8ffa21919a8902180a9f76bd4bba4edb3220
Gonzalo santiago martinez <simpleverso@users.noreply.github.com>
2020-05-23 23:10:38 -0500

    test de video

---
+++ videoTest/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace videoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_contra = new System.Windows.Forms.TextBox();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.txt_puerto = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(28, 33);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(282, 264);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 70);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(351, 33);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(100, 20);
+            this.txt_usuario.TabIndex = 4;
+            this.txt_usuario.Text = "Service";
+            // 
+            // txt_contra
+            // 
+            this.txt_contra.Location = new System.Drawing.Point(351, 61);
+            this.txt_contra.Name = "txt_contra";
+            this.txt_contra.Size = new System.Drawing.Size(100, 20);
+            this.txt_contra.TabIndex = 5;
+            this.txt_contra.Text = "Service.1";
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(351, 96);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(100, 20);
+            this.txt_ip.TabIndex = 6;
+            this.txt_ip.Text = "172.16.136.71";
+            // 
+            // txt_puerto
+            // 
+            this.txt_puerto.Location = new System.Drawing.Point(351, 136);
+            this.txt_puerto.Name = "txt_puerto";
+            this.txt_puerto.Size = new System.Drawing.Size(100, 20);
+            this.txt_puerto.TabIndex = 7;
+            this.txt_puerto.Text = "554";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_puerto);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.txt_contra);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_contra;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.TextBox txt_puerto;
+    }
+}
+
---
+++ videoTest/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace videoTest
+{
+    public partial class Form1 : Form
+    {
+        Capture cap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cap = new Capture("rtsp://"+txt_usuario.Text+":"+txt_contra.Text+"@"+txt_ip.Text+":"+txt_puerto.Text+"");
+                cap.ImageGrabbed += ProcessFrame;
+                cap.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            Mat image = new Mat();
+            cap.Retrieve(image);
+            imageBox1.BackgroundImage = image.Bitmap;
+        }
+    }
+}
